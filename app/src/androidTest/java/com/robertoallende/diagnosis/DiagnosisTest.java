e49bddb46184e69ecfeb03f11516cd808afd0802
Roberto Allende <rallende@gmail.com>
2016-08-24 15:10:03 -0300

    Adding initial tests.

---
+++ app/src/androidTest/java/com/robertoallende/diagnosis/DiagnosisTest.java
@@ -0,0 +1,36 @@
+package com.robertoallende.diagnosis;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.action.ViewActions.click;
+
+import com.robertoallende.diagnosis.view.DiagnosisActivity;
+
+@RunWith(AndroidJUnit4.class)
+public class DiagnosisTest {
+
+    @Rule
+    public ActivityTestRule<DiagnosisActivity> mDiagnosisActivityTestRule =
+            new ActivityTestRule<DiagnosisActivity>(DiagnosisActivity.class);
+
+    @Test
+    public void emptyCase() {
+        onView(withId(R.id.fab)).perform(click());
+        onView(withId(R.id.question_activity)).check(matches(isDisplayed()));
+        onView(withId(R.id.choice_1)).perform(click());
+        onView(withId(R.id.result_activity)).check(matches(isDisplayed()));
+        onView(withId(R.id.close_result)).perform(click());
+        onView(withId(R.id.diagnosis_activity)).check(matches(isDisplayed()));
+
+
+    }
+}
+
